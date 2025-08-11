9c8a4e63ea4ed4cc4c965b5ec1eae86a0ab510f5
allen <boseallen192@gmail.com>
2025-08-11 22:33:21 +0530

    added form validation!

---
+++ Unohana/Dtos/SignUpDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unohana.Dtos
+{
+    public class SignUpDto
+    {
+        [Required]
+        [MinLength(8, ErrorMessage = "password must be greater than 8 characters!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "passwords must match!")]
+        [DataType(DataType.Password)]
+        public string CheckPassword { get; set; } = string.Empty;
+    }
+}
